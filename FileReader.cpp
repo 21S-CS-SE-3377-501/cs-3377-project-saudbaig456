6ba1410b614a4828ae927fb96644f84c1cd795e8
Saud Baig <saudsamibaig@gmail.com>
2021-03-22 21:47:52 -0500

    Fixed Compiling and Refactored Added CMakeLists

--- FileReader.cpp
+++ FileReader.cpp
@@ -1,5 +1,54 @@
 //
 // Created by saudbaig on 3/22/21.
 //
-
+//add minimal error handling to functions
 #include "FileReader.h"
+#include <iostream>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <cstdio>
+#include <ctime>
+
+FileReader::FileReader(const char *source) {
+    FileReader::fd = open(source, O_RDONLY, S_IRUSR);
+    readInt(&(FileReader::numEntries));
+}
+
+void FileReader::readInt(int* buffer) {
+    read(FileReader::fd, buffer, sizeof(int));
+}
+
+void FileReader::readTime(time_t* buffer) {
+    read(FileReader::fd, buffer, sizeof(time_t));
+}
+
+void FileReader::readString(char* buffer) {
+    read(FileReader::fd, buffer, 50 * sizeof(char));
+}
+
+void FileReader::readFloat(float* buffer) {
+    read(fd, buffer, sizeof(float));
+}
+
+FileReader::~FileReader() {
+    close(FileReader::fd);
+}
+
+
+int FileReader::getFd() const {
+    return fd;
+}
+
+void FileReader::setFd(int fd) {
+    FileReader::fd = fd;
+}
+
+int FileReader::getNumEntries() const {
+    return numEntries;
+}
+
+void FileReader::setNumEntries(int numEntries) {
+    FileReader::numEntries = numEntries;
+}
--- FileReader.h
+++ FileReader.h
@@ -1,8 +1,28 @@
 //
 // Created by saudbaig on 3/22/21.
 //
+#ifndef INC_21S_CS3377_PROJECT_FILEREADER_H
+#define INC_21S_CS3377_PROJECT_FILEREADER_H
+#include <cstdio>
+#include <ctime>
 
-#ifndef PROJECTTEMPLATE_FILEREADER_H
-#define PROJECTTEMPLATE_FILEREADER_H
+class FileReader {
 
+public:
+    FileReader(const char *source);
+    void readInt(int* buffer);
+    void readTime(time_t* buffer);
+    void readString(char* buffer);
+    void readFloat(float* buffer);
+    int getFd() const;
+    void setFd(int fd);
+    int getNumEntries() const;
+    void setNumEntries(int numEntries);
+    ~FileReader();
+
+private:
+    int fd;
+    int numEntries;
+
+};
 #endif //PROJECTTEMPLATE_FILEREADER_H
